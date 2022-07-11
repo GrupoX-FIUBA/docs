8344d7e91bada76bff4f80c09e4e4e02ee26abb5
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-07-11 15:51:56 -0300

    Remove broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Spotifiuby docs',
   tagline: 'Taller de programación II - FIUBA',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.spotifiuby.com.ar',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'GrupoX-FIUBA', // Usually your GitHub org/user name.
+  projectName: 'Spotifiuby', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'es',
+    locales: ['es'],
   },
 
   presets: [
